7f0042cf96de970abef16d104b6014966d166ac0
KvitD <kvit.d.o@gmail.com>
2025-02-28 13:52:58 +0200

    add: conection to mysql

--- backend/index.js
+++ backend/index.js
@@ -1,18 +1,38 @@
 import express from 'express';
-import mysql from 'mysql';
+import mysql from 'mysql2';
 
 const app = express();
+
+
 
 const db = mysql.createConnection({
     host: 'localhost',
     user: 'root',
-    password: '123456',
-    database: 'test',
+    password: 'edcVxswqaz132435',
+    database: 'dzencodedb',
 
 })
 
+
+db.connect(err => {
+    if (err) {
+        console.error('Database connection failed:', err.stack);
+        return;
+    }
+    console.log('Connected to database');
+});
+
+
 app.get("/", (req, res) => {
     res.json("Hello this is the backend!");
+})
+
+app.get("/products", (req, res) => {
+    const q = "SELECT * FROM products";
+    db.query(q, (err, result) => {
+        if (err) return res.json(err);
+        return res.json(result);
+    })
 })
 
 app.listen(8080,()=>{
