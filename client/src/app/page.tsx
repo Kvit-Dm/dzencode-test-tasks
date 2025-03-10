44d366da1418eef58670da97d93e136258613161
KvitD <kvit.d.o@gmail.com>
2025-03-10 13:50:58 +0200

    add: app/page grid layout

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,32 +1,30 @@
 'use client';
 
-import styles from "./page.module.css";
+import styles from './page.module.css';
 import { Provider } from 'react-redux';
-import {store} from '@/redux/store'
+import { store } from '@/redux/store';
 
 // import React, { Suspense, useEffect, useState, lazy } from 'react';
 
 export default function Home() {
-  return(
-      <Provider store={store}>
-        <div className={styles['main-grid']}>
-          <div className={styles['side-bar']}>
-            <h3>Filters</h3>
-            <div />
-          </div>
-          <div className={styles['header']}>
-            <h1>Car Dealer</h1>
-          </div>
-          <div className={styles['main-block']}>
-            <div >
-              {/*<AlwaysLoadingComponent/>*/}
-              <main>
-                {/*<MainBlock/>*/}
-              </main>
-              {/*<MainBlock/>*/}
-            </div>
+  return (
+    <Provider store={store}>
+      <div className={styles['main-grid']}>
+        <div className={styles['side-bar']}>
+          <h3>Menu</h3>
+          <div />
+        </div>
+        <div className={styles['header']}>
+          <h2>INVENTORY</h2>
+        </div>
+        <div className={styles['main-block']}>
+          <div>
+            {/*<AlwaysLoadingComponent/>*/}
+            <main>{/*<MainBlock/>*/}</main>
+            {/*<MainBlock/>*/}
           </div>
         </div>
-      </Provider>
-  ) ;
+      </div>
+    </Provider>
+  );
 }
