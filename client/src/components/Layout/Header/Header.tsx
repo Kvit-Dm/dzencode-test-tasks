4e023d9971ac4ff396d446b8bf9ec4453945a753
KvitD <kvit.d.o@gmail.com>
2025-08-17 15:19:45 +0300

    chek: before radix

--- client/src/components/Layout/Header/Header.tsx
+++ client/src/components/Layout/Header/Header.tsx
@@ -5,8 +5,13 @@ import styles from './Header.module.scss';
 export default function LayoutHeader() {
   return (
     <div className={styles.header}>
-      <Image className={styles.headerLogo} src={logo} alt={'logo'} />
-      <h2 className={styles.headerTitle}>INVENTORY</h2>
+      <div className="d-flex align-items-center">
+        <Image className={styles.headerLogo} src={logo} alt={'logo'} />
+        <h2 className={styles.headerTitle}>INVENTORY</h2>
+      </div>
+      <div>
+        <p>some text ?</p>
+      </div>
     </div>
   );
 }
